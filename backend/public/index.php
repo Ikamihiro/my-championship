27839b85669059a62b78d76cb51e7dd32d8fb0da
João G. Silva <joogabrielsntn66@gmail.com>
2021-09-26 09:03:16 -0400

    Adicionando suporte para Prefligth Request (CORS)

--- backend/public/index.php
+++ backend/public/index.php
@@ -10,7 +10,9 @@ require_once __DIR__ . '/../config/helpers.php';
 require_once __DIR__ . '/../config/env.php';
 require_once __DIR__ . '/../config/database.php';
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, PATCH");
 
 use App\Controllers\Api\{
     AuthController,
